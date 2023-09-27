6e00650321290a445ba82dfb87a95cda4bd63323
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-09-27 13:14:51 -0400

    insert router.js

---
+++ router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Rota inicial
+router.get('/', (req, res) => {
+   res.type('html').send(html);
+});
+
+
+
+module.exports = router;
