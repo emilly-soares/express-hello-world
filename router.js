b71c1da25bba3095006c7b4e68e88fff3b950098
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-09-27 13:16:16 -0400

    endpoint 1

--- router.js
+++ router.js
@@ -6,6 +6,16 @@ router.get('/', (req, res) => {
    res.type('html').send(html);
 });
 
+// Rota /req
+router.get('/req', (req, res) => {
+   console.log('Just got a request!');
+   res.send('Yo!');
+});
 
+// Desafio 1: Rota /meunome
+router.get('/meunome', (req, res) => {
+   const meuNome = 'Meu nome é Emilly Soares Santos';
+   res.send(meuNome);
+});
 
 module.exports = router;
