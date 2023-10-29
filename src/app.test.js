e3fe9737bf18a995c68282e29d89744e5caee86c
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-28 22:26:02 -0400

    update test

--- src/app.test.js
+++ src/app.test.js
@@ -2,18 +2,7 @@ const request = require('supertest');
 const { app } = require('./app');
 
 describe('Test Endpoints', () => {
-    it('should return Hello World from /hello', async () => {
-      const response = await request(app).get('/hello');
-      expect(response.status).toBe(200);
-      expect(response.body.message).toBe('Hello World!');
-    });
-  
-    it('should return Yo! from /req', async () => {
-      const response = await request(app).get('/req');
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('Yo!');
-    });
-  
+    
     it('should return the author name from /meunome', async () => {
       const response = await request(app).get('/meunome');
       expect(response.status).toBe(200);
