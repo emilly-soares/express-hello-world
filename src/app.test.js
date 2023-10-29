14364629787b25b9a782e1aad96f6f96513b2d77
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-28 22:54:05 -0400

    update app.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
-const { app } = require('./app');
+const { app } = require('../app');
 
 describe('Test Endpoints', () => {
-    
+
     it('should return the author name from /meunome', async () => {
       const response = await request(app).get('/meunome');
       expect(response.status).toBe(200);
@@ -12,12 +12,12 @@ describe('Test Endpoints', () => {
     it('should return a list of pokemons from /pokemons', async () => {
       const response = await request(app).get('/pokemons');
       expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(10); // Check the length or structure of the response
+      expect(response.body).toHaveLength(10); 
     });
   
     it('should return a list of series from /series', async () => {
       const response = await request(app).post('/series');
       expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(3); // Check the length or structure of the response
+      expect(response.body).toHaveLength(3); 
     });
   });
